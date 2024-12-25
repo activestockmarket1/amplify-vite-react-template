491c3d38309ac4930fbfba176cd4d082822b1fa8
Prakash Shanmugam <prakashshanmugam@Prakashs-MacBook-Pro.local>
2024-12-25 03:28:45 -0800

    added amplifyconfiguration change 2

--- src/amplifyconfiguration.ts
+++ src/amplifyconfiguration.ts
@@ -1,9 +1,10 @@
+// src/amplifyconfiguration.ts
 const amplifyconfig = {
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-      signUpVerificationMethod: 'code',
+      signUpVerificationMethod: 'code' as 'code', // Type assertion to fix the error
       loginWith: {
         email: true,
         username: false,
